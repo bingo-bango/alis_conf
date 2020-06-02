49df0632b6031a20b499700319b41d8b4fadd3bc
bingo-bango <web@petercairns.co.uk>
2020-06-02 15:39:12 +0900

    Create post_install.sh

---
+++ post_install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# This script performs the following post-installation tasks:
+# -configure megasync
+# -deploy docker containers
+# -restore and enable systemd scheduled tasks
+# -restore scripts
+# etc
+
+
+# docker containers:
+# portainer
+# pihole
+# huginn
+# transmission
+# mosquitto
+# openhab
+# jabber
+# whoogle https://github.com/benbusby/whoogle-search
+# post.io (mail server)
+# openvpn

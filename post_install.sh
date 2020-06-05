d825861c19ec6a16aae37134f9bcbafe1da90e19
bingo-bango <web@petercairns.co.uk>
2020-06-06 07:51:11 +0900

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/env bash
 # This script performs the following post-installation tasks:
+# -configure data drive
 # -configure megasync
 # -deploy docker containers
 # -restore and enable systemd scheduled tasks
 # -restore scripts
 # etc
+
+function mount_data() {
+  # configure and mount data drive
+  echo "Creating data drive mount point..."
+  mkdir /mnt/data
+  chmod 777 -R /mnt/data
+  echo "Unlocking data drive..."
+  cryptsetup luksOpen /dev/disk/by-uuid/<<INSERT UUID>> encrypted_data
+  echo "Mounting data drive..."
+  mount /dev/mapper/encrypted_data /mnt/data
+}  
 
 
 # docker containers:

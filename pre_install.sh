b08ba5312c70a94e70c5c511fb26aff182e49499
bingo-bango <web@petercairns.co.uk>
2020-07-19 12:30:39 +0200

    Update pre_install.sh
    Add option for local cache instead of hard coded

--- pre_install.sh
+++ pre_install.sh
@@ -5,17 +5,29 @@
 # 3) pulls latest alis script
 # 4) pulls customised config files
 
-# set host name for pacman cache server
-echo "172.16.0.42 pacserver" >> /etc/hosts
+# Set this to 1 if a local pacman cache server is being used
+Local_Cache=0
 
-# set up fstab with pacman share
-echo "pacserver:/var/cache/pacman/pkg	/var/cache/pacman/pkg		nfs	noauto,nofail,x-systemd.automount,x-systemd.device-timeout=30,_netdev 0 0" >> /etc/fstab
+# Set the local cache IP if needed
+Local_Cache_IP="192.168.1.123"
 
-# mount share
-mount /var/cache/pacman/pkg
+ if [ $Local_Cache = 1 ]
+ then
+ 
+  # set host name for pacman cache server
+  echo "$Local_Cache_IP pacserver" >> /etc/hosts
 
-# notify
-echo "network pacman cache is ready!"
+  # set up fstab with pacman share
+  echo "pacserver:/var/cache/pacman/pkg	/var/cache/pacman/pkg		nfs	noauto,nofail,x-systemd.automount,x-systemd.device-timeout=30,_netdev 0 0" >> /etc/fstab
+
+  # mount share
+  mount /var/cache/pacman/pkg
+
+  # notify
+  echo "network pacman cache is ready!"
+  fi
+  
+
 
 # pull latest alis download script from the repo
 wget https://raw.githubusercontent.com/picodotdev/alis/master/download.sh | bash
@@ -26,7 +38,10 @@ wget https://raw.githubusercontent.com/bingo-bango/alis_conf/master/alis_latitud
 # pull latest alis using download script
 bash download.sh
 
-# patch alis so that local pacman cache is used
-sed -i 's/pacstrap/pacstrap -c/g' alis.sh
+ if [ $Local_Cache = 1 ]
+ then
+  # patch alis so that local pacman cache is used
+  sed -i 's/pacstrap/pacstrap -c/g' alis.sh
+  fi
 
 echo "Operation complete. Replace the alis.conf file with the customized one and run: bash alis.sh"

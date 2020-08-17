c1dfa7e3e227d4be3a2b9f788ec36c1c14304148
bingo-bango <web@petercairns.co.uk>
2020-08-17 21:44:12 +0200

    Update mount_data.sh

--- mount_data.sh
+++ mount_data.sh
@@ -8,7 +8,7 @@ function mount_data() {
   chmod 777 -R /mnt/data
   echo "Unlocking data drive..."
   cryptsetup luksOpen /dev/disk/by-uuid/73cd4968-6492-4f07-9780-e12236581d5e encrypted_data
-  #echo "lat:/srv/nfs/data	/mnt/data		nfs	defaults,rsize=32768,wsize=32768,timeo=900,retrans=5,_netdev	0 0" >> /etc/fstab
+  echo "lat:/srv/nfs/data	/mnt/data		nfs	defaults,rsize=32768,wsize=32768,timeo=900,retrans=5,_netdev	0 0" >> /etc/fstab
   echo "Mounting data drive..."
   mount /dev/mapper/encrypted_data /mnt/data
 }

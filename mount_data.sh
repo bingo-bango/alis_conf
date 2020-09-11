f5cccb086a124edd5b4cd6c22b338d56dc31f98d
bingo-bango <web@petercairns.co.uk>
2020-09-11 20:38:51 +0200

    Update mount_data.sh
    change mount points

--- mount_data.sh
+++ mount_data.sh
@@ -4,13 +4,13 @@
 function mount_data() {
   # configure and mount data drive
   echo "Creating data drive mount point..."
-  mkdir /mnt/data
-  chmod 777 -R /mnt/data
+  mkdir /data
+  chmod 777 -R /data
   echo "Unlocking data drive..."
   cryptsetup luksOpen /dev/disk/by-uuid/73cd4968-6492-4f07-9780-e12236581d5e encrypted_data
-  echo "lat:/srv/nfs/data	/mnt/data		nfs	defaults,rsize=32768,wsize=32768,timeo=900,retrans=5,_netdev	0 0" >> /etc/fstab
+  echo "lat:/srv/nfs/data	/data		nfs	defaults,rsize=32768,wsize=32768,timeo=900,retrans=5,_netdev	0 0" >> /etc/fstab
   echo "Mounting data drive..."
-  mount /dev/mapper/encrypted_data /mnt/data
+  mount /dev/mapper/encrypted_data /data
 }
 
  
